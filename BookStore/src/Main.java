c0c53c8976d42bbfbb17644c758b35626d061f67
Duuuu <vohoangvu1201@gmail.com>
2025-05-10 08:53:29 +0700

    Đang làm

--- BookStore/src/Main.java
+++ BookStore/src/Main.java
@@ -4,7 +4,8 @@ import Config.SeedData;
 public class Main {
     public static void main(String[] args) {
         GlobalException.setup();
-        new UI.LoginFrame();
-        new SeedData();
+//        new UI.LoginFrame();
+//        new SeedData();
+        new UI.BookDetailForm(7);
     }
 }
